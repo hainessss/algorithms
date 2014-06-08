0f663cf89f8b316946f6d9d36f63df97e1103938
alex haines <hainesaxiv@gmail.com>
2014-06-07 22:41:42 -0700

    passed tests on swap and find sum 2 and 3

--- set1/set1.rb
+++ set1/set1.rb
@@ -1,10 +1,35 @@
 module Set1
+  # O(n)
   def self.swap_small(array)
+    min = array.min
+    array[array.index(min)] = array[0]
+    array[0] = min
+    return array
   end
 
+  # O(n^2)
   def self.find_sum_2(array, sum = 0)
+    array.each do |val|
+      array.each do |value|
+        if val + value == 0 #&& ind != index
+          return true
+        end
+      end
+    end
+    false
   end
 
+  #O()
   def self.find_sum_3(array)
+    array.each do |v|
+      array.each do |val|
+        array.each do |value|
+          if val + value + v == 0 #&& ind != index
+            return true
+          end
+        end
+      end
+    end
+    false
   end
 end
