2652e764f8af928913d981ee7b3360118d64b819
alex haines <hainesaxiv@gmail.com>
2014-06-10 10:13:29 -0700

    working war, implemented two array data structure

--- WAR/spec/war_spec.rb
+++ WAR/spec/war_spec.rb
@@ -3,3 +3,134 @@ require 'rspec'
 require 'pry-debugger'
 require_relative '../war.rb'
 
+
+describe Card do
+  describe 'intialize' do
+    it 'is a card with rank, value, and suit' do
+      card = Card.new('J', 11, 'Spades')
+
+      expect(card.rank.downcase!).to eq 'j'
+      expect(card.value).to eq 11
+      expect(card.suit.downcase!).to eq 'spades'
+      expect(card).to be_a(Card)
+    end
+  end
+end
+
+describe Deck do
+  let(:deck1) { Deck.new }
+
+  describe 'intialize' do
+    # let(:deck) { Deck.new }
+    it "to be an array" do
+      expect(deck1.deck).to be_a(Array)
+    end
+  end
+
+  describe 'add_card' do
+    it "should add one card to store" do
+      deck1.create_52_card_deck
+      added_card = deck1.deal_card
+      deck1.add_card(added_card)
+      # binding.pry
+      expect(deck1.store[0]).to eq added_card
+    end
+  end
+
+  describe 'shuffle' do
+    it "should randomize the deck" do
+      deck1.create_52_card_deck
+      before_shuffled = deck1.deck
+      after_shuffled = deck1.shuffle
+
+      expect(after_shuffled).not_to eq before_shuffled
+    end
+  end
+
+  describe '.deal_card' do
+    it "should return the top card of the deck" do
+      deck1.create_52_card_deck
+      dealt_card = deck1.deck[0]
+      expect(deck1.deal_card).to eq dealt_card
+    end
+
+    # it "should store the cards in a placeholder array" do
+    #   deck1.create_52_card_deck
+    #   51.times { deck1.deal_card }
+    #   expect(deck1.store.length).to eq 51
+    # end
+
+    # it "should circle through the deck" do
+    #   deck1.create_52_card_deck
+    #   dealt_card = deck1.deck[0]
+    #   52.times { deck1.deal_card }
+    #   # binding.pry
+    #   expect(deck1.deal_card).to eq dealt_card
+    # end
+  end
+
+  describe '.create_52_card_deck' do
+    it 'is an array with 52 cars' do
+      deck1.create_52_card_deck
+      expect(deck1.deck.length).to eq 52
+    end
+  end
+end
+
+ # binding.pry
+
+describe Player do
+  describe 'intialize' do
+    it 'should have a name and a deck' do
+      pete = Player.new('pete')
+
+      expect(pete.name).to eq 'pete'
+      expect(pete.hand).to be_a Deck
+    end
+  end
+end
+
+describe War do
+  describe 'intialize' do
+    it 'should take in two players' do
+      game1 = War.new('alex', 'pete')
+
+      expect(game1.player1.name).to eq 'alex'
+      expect(game1.player2.name).to eq 'pete'
+      expect(game1.player1).to be_a Player
+      expect(game1.player2).to be_a Player
+    end
+
+    it 'should shuffle and pass out the cards to each player' do
+      game1 = War.new('alex', 'pete')
+      # binding.pry
+      expect(game1.player1.hand.deck.length).to eq 26
+      expect(game1.player1.hand.deck.length).to eq 26
+    end
+  end
+
+  describe 'play_game' do
+    it 'determines a winner and redelegates cars' do
+      game1 = War.new('alex', 'pete')
+      game1.player1.hand.deck = [Card.new('J', 11, 'spades')]
+      game1.player2.hand.deck = [Card.new('2', 2, 'spades')]
+      game1.play_game
+
+      expect(game1.player1.hand.deck.length).to eq 2
+    end
+
+    it 'ends the game when one player is out of cards' do
+      game1 = War.new('alex', 'pete')
+      game1.player1.hand.deck = [Card.new('J', 11, 'spades')]
+      game1.player2.hand.deck = [Card.new('2', 2, 'spades')]
+      # binding.pry
+      game1.play_game
+      expect(game1.play_game).to eq game1.player1
+    end
+  end
+end
+
+describe WarAPI do
+  describe 'play_turn' do
+  end
+end
--- WAR/war.rb
+++ WAR/war.rb
@@ -1,5 +1,9 @@
+require 'pry-debugger'
+
+
 # This class is complete. You do not need to alter this
 class Card
+  attr_reader :rank, :value, :suit
   # Rank is the rank of the card, 2-10, J, Q, K, A
   # Value is the numeric value of the card, so J = 11, A = 14
   # Suit is the suit of the card, Spades, Diamonds, Clubs or Hearts
@@ -12,35 +16,71 @@ end
 
 # TODO: You will need to complete the methods in this class
 class Deck
-  attr_accessor :deck
+  attr_accessor :deck, :store
+  attr_reader :index
   def initialize
-    @deck = nil # Determine the best way to hold the cards
+    @deck = [] # Determine the best way to hold the cards
+    @store = []
+    @index = 0
   end
 
   # Given a card, insert it on the bottom your deck
   def add_card(card)
-
+    @store << card
   end
 
   # Mix around the order of the cards in your deck
   def shuffle # You can't use .shuffle!
-
+    52.times do |i|
+      j = rand(51)
+      @deck[i], @deck[j] = @deck[j], @deck[i]
+    end
   end
 
   # Remove the top card from your deck and return it
   def deal_card
+    dealt_card = deck[@index]
+    deck[@index] = nil
+    @index += 1
 
+    # if deck[@index] == nil
+    #   @deck = @store
+    #   @store = []
+    #   @index = 0
+    # end
+
+    dealt_card
+    # binding.pry
+  end
+
+  def check_deck
+    if deck[@index] == nil
+      @deck = @store
+      @store = []
+      @index = 0
+    end
   end
 
   # Reset this deck with 52 cards
   def create_52_card_deck
-
+    temp_value = 11
+    ['hearts','spades','diamonds','clubs'].each do |suit|
+      (2..10).each do |value|
+        @deck <<  Card.new(value, value, suit)
+      end
+      ['J','Q','K','A'].each do |face|
+        @deck <<  Card.new(face, temp_value, suit)
+        temp_value += 1
+      end
+    end
   end
-
 end
 
 # You may or may not need to alter this class
 class Player
+  attr_reader :name
+  attr_accessor :hand
+
   def initialize(name)
     @name = name
     @hand = Deck.new
@@ -49,19 +89,85 @@ end
 
 
 class War
+  attr_reader :player1, :player2
+
   def initialize(player1, player2)
     @deck = Deck.new
     @player1 = Player.new(player1)
     @player2 = Player.new(player2)
-    # You will need to shuffle and pass out the cards to each player
+
+    @deck.create_52_card_deck
+    @deck.shuffle
+
+    26.times do
+      @player1.hand.deck << @deck.deck.pop
+      @player2.hand.deck << @deck.deck.pop
+    end
   end
 
   # You will need to play the entire game in this method using the WarAPI
   def play_game
-    # WarAPI.play_turn()
-  end
-end
+      until @player1.hand.deck == [] || @player2.hand.deck == []
 
+        deck_p1 = @player1.hand
+        deck_p2 = @player2.hand
+
+        # deck_p1.check_deck
+        # deck_p2.check_deck
+
+        draw_p1 = deck_p1.deal_card
+        draw_p2 = deck_p2.deal_card
+        @turn_count = 0
+
+        puts "#{@player1.name} played a #{draw_p1.rank}"
+        puts "#{@player2.name} played a #{draw_p2.rank}"
+        # binding.pry
+        result = WarAPI.play_turn(@player1, draw_p1, @player2, draw_p2)
+        result[player1].each { |v| deck_p1.add_card(v) ; puts "player 1 won a #{v.rank} for his hand" }
+        result[player2].each { |v| deck_p2.add_card(v) ; puts "player 1 won a #{v.rank} to his hand" }
+
+        deck_p1.check_deck
+        deck_p2.check_deck
+
+        # binding.pry
+        @turn_count += 1
+      end
+      get_winner
+  end
+
+  # def draw
+  #   deck_p1 = @player1.hand
+  #   deck_p2 = @player2.hand
+
+  #   draw_battle = []
+  #   # draw_battle << 3.times { @player1.hand.deal_card }
+  #   # draw_battle << 3.times { @player2.hand.deal_card }
+
+  #   draw_p1 = deck_p1.deal_card
+  #   draw_p2 = deck_p2.deal_card
+
+  #   result = WarAPI.play_turn(@player1, draw_p1, @player2, draw_p2)
+  #   result[player1].each { |v| deck_p1.add_card(v) ; puts "player 1 won a #{v} for his hand" }
+  #   result[player2].each { |v| deck_p2.add_card(v) ; puts "player 1 won a #{v} to his hand" }
+
+  #   if result[player1] = []
+  #     draw_battle.each { |v| deck_p1.add_card(v) ; puts "player 1 won a #{v} for his hand" }
+  #   else
+  #     draw_battle.each { |v| deck_p2.add_card(v) ; puts "player 1 won a #{v} to his hand" }
+  #   end
+  # end
+
+  def get_winner
+    if @player1.hand.deck.length == []
+      puts "#{@player2.name} has one the game in #{@turn_count} turns"
+      @player2
+    else
+      puts "#{@player1.name} has one the game in #{@turn_count} turns"
+      @player1
+    end
+  end
+
+end
 
 class WarAPI
   # This method will take a card from each player and
@@ -69,10 +175,36 @@ class WarAPI
   def self.play_turn(player1, card1, player2, card2)
     if card1.value > card2.value
       {player1 => [card1, card2], player2 => []}
-    elsif card2.value > card1.value || Rand(100).even?
+    elsif card2.value > card1.value
       {player1 => [], player2 => [card2, card1]}
     else
-      {player1 => [card1, card2], player2 => []}
+      {player1 => [], player2 => []}
+      #draw(player1, player2)
     end
   end
+
+  # def draw(player1, player2)
+  #   deck_p1 = player1.hand
+  #   deck_p2 = player2.hand
+
+  #   draw_battle = []
+  #   # draw_battle << 3.times { player1.hand.deal_card }
+  #   # draw_battle << 3.times { player2.hand.deal_card }
+
+  #   draw_p1 = deck_p1.deal_card
+  #   draw_p2 = deck_p2.deal_card
+
+  #   result = WarAPI.play_turn(player1, draw_p1, player2, draw_p2)
+  #   result[player1].each { |v| deck_p1.add_card(v) ; puts "player 1 won a #{v} for his hand" }
+  #   result[player2].each { |v| deck_p2.add_card(v) ; puts "player 1 won a #{v} to his hand" }
+
+  #   if result[player1] = []
+  #     draw_battle.each { |v| deck_p1.add_card(v) ; puts "player 1 won a #{v} for his hand" }
+  #   else
+  #     draw_battle.each { |v| deck_p2.add_card(v) ; puts "player 1 won a #{v} to his hand" }
+  #   end
+  # end
+
 end
+
+# binding.pry
